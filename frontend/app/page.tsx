d69ae06dc8c7e8c285e63f87968e68bdfc97c11d
comros <itscomros@gmail.com>
2026-05-16 18:25:19 +0200

    connect: supabase

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,26 @@
 import { AppShell } from '@/components/app-shell'
 import { DashboardContent } from '@/components/dashboard-content'
 
+/*
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  )
+}
+*/
+
 export default function DashboardPage() {
   return (
     <AppShell>
